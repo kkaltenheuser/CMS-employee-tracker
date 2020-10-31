5a9ab7561b9269f5403c6dfcf4e822489349fca3
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-10-31 14:29:30 -0400

    NPM init + build other tables in SQL

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS "";
+CREATE DATABASE "";
+-- LETS DATABASE USE DATA--
+USE "";
 -- table one--
 -- department --
 CREATE TABLE department(
@@ -13,10 +17,15 @@ PRIMARY KEY (id)
 -- table two --
 -- role--
 CREATE TABLE roles (
-    -- id --
+ -- id --
+id INT NOT NULL AUTO_INCREMENT,
 -- title --
+role_title VARCHAR (30) NOT NULL,
 -- salary --
+salary DECIMAL (10,2) NOT NULL,
 -- department id --
+department_id INT NOT NULL,
+PRIMARY KEY (id)
 )
 
 
@@ -25,8 +34,15 @@ CREATE TABLE roles (
 -- employee --
 CREATE TABLE employee (
 -- id --
+id INT NOT NULL AUTO_INCREMENT,
 -- first name --
+first_name VARCHAR(30) NOT NULL,
 -- last name --
+last_name VARCHAR(30) NOT NULL,
 -- role id --
+role_id INT NOT NULL,
 -- manager id --
+manager_id INT NULL,
+PRIMARY KEY (id)
+
 )
