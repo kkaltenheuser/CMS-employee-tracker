3028c04c460e879ea4891009ff093c207e0187ec
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-10-31 14:13:42 -0400

    added sql schema + first table

---
+++ schema.sql
@@ -0,0 +1,32 @@
+-- table one--
+-- department --
+CREATE TABLE department(
+-- id --
+id INT NOT NULL AUTO_INCREMENT,
+-- name / varchar 30 --
+department_name VARCHAR (30) NOT NULL,
+PRIMARY KEY (id)
+)
+
+
+
+-- table two --
+-- role--
+CREATE TABLE roles (
+    -- id --
+-- title --
+-- salary --
+-- department id --
+)
+
+
+
+-- table three
+-- employee --
+CREATE TABLE employee (
+-- id --
+-- first name --
+-- last name --
+-- role id --
+-- manager id --
+)
